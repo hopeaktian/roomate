e131548b471b79939b41ef43edbc17268402a8cc
hopeaktian <hopeaktian@gmail.com>
2018-06-07 18:06:57 +0800

    fix flask bug

--- app/run.py
+++ app/run.py
@@ -6,12 +6,11 @@ date:2018/6/7 13:26
 author:    peak
 description:
 """
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', title="袋鼠邻居")
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

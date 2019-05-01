650a94a996890439b5a25401f1e15b8341f90a6a
hopeaktian <hopeaktian@gmail.com>
2019-05-01 22:53:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    SECRET_KEY = '7a71ea10c4af9f16f020488e115b32fd'
+    SECRET_KEY = ''
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:yourpassword@120.79.139.82:3306/flask_learn01"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:yourpassword@IP:3306/database_name"
